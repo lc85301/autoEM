93dd2dbe8e612b4bd3543059383f012cc28687db
YodaLee <lc85301@gmail.com>
2012-10-29 09:17:15 +0000

    change the main function of autoEM project

--- autoEM.py
+++ autoEM.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7
+
 ##################################################################################
 #-*- coding: utf-8 -*-
 # 
@@ -7,7 +8,7 @@
 # Copyright (C) 2012 -  You-Tang Lee (YodaLee) <lc85301@gmail.com>
 # All Rights reserved.
 #
-# This file is part of project: autoEM
+# This file is part of project: autoEM.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,27 +25,20 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ##################################################################################
 
-import os
-import os.path
+#builtin library
 import sys
-import getpass
-import traceback
-import time
-import collections
+import os.path
 from optparse import OptionParser, make_option
-
-#third part library
-import paramiko
+#self define library
 
 program = sys.argv[0]
-LANUCH_DIR = os.path.dirname(os.path.abspath(sys.path[0]))
-
+LAUNCH_DIR = os.path.dirname(os.path.abspath(sys.path[0]))
 # If launched from source directory
 if program.startswith('./') or program.startswith('bin/'):
-    sys.path.insert(0, LANUCH_DIR)
+    sys.path.insert(0, LAUNCH_DIR)
 
-program_name = sys.argv[0]
-program_version = '0.1.0'
+from autoEM.misc import *
+
 def version():
 	sys.stderr.write(
 '%s Ver %s\n\
@@ -54,106 +48,6 @@ This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n\
 Written by YodaLee <lc85301@gmail.com>.\n' % (program_name, program_version)
 )
-
-#sftp = paramiko.SFTPClient.from_transport(t)
-#try:
-#	sftp.mkdir("autoEM")
-#except IOError:
-#	print '(autoEM folder already exists)'
-#sftp.put(filename, filename)
-
-default_command="""
-#!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin 
-export PATH
-
-source ~/.bashrc
-rm -rf ~/autoEM/sondata/%s
-em ~/autoEM/%s.son
-"""
-def exec_command(filename):
-	#setup log file
-	info = get_user_info()
-	try:
-		ssh = paramiko.SSHClient()
-		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		ssh.connect(info.hostname, username=info.username, password = info.password)
-		stdin, stdout, stderr = ssh.exec_command('source ~/.bashrc; nohup em ~/autoEM/%s </dev/null >em.log 2>&1 &' % filename)
-		#stdout.readline()
-		stderr.readline()
-	except exception, e:
-		print '*** Caught exception %s: %s ***' % (e.__class__, e)
-		traceback.print_exc()
-	finally:
-		ssh.close()
-		sys.exit(1)
-
-def get_user_info():
-	"""get username and password"""
-	# get hostname
-	hostname = ''
-	username = ''
-	password = ''
-	port = 22
-	hostname = raw_input('Hostname: ')
-	if len(hostname) == 0:
-		print '*** Hostname required. ***'
-		sys.exit(1)
-	if hostname.find('@') >= 0 :
-		username, hostname = hostname.split('@')
-	if hostname.find(':') >= 0:
-		hostname, portstr = hostname.split(':')
-		port = int(portstr)
-	## get username
-	if username == '':
-		default_username = getpass.getuser()
-		username = raw_input('Username [%s]: ' % default_username)
-		if len(username) == 0:
-			username = default_username
-	password = getpass.getpass('Password for %s@%s: ' % (username, hostname))
-
-	# get host key, if we know one
-	hostkeytype = None
-	hostkey = None
-	try:
-		host_keys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
-	except IOError:
-		try:
-			# try ~/ssh/ too, because windows can't have a folder named ~/.ssh/
-			host_keys = paramiko.util.load_host_keys(os.path.expanduser('~/ssh/known_hosts'))
-		except IOError:
-			print '*** Unable to open host keys file ***'
-			host_keys = {}
-	if host_keys.has_key(hostname):
-		hostkeytype = host_keys[hostname].keys()[0]
-		hostkey = host_keys[hostname][hostkeytype]
-		print 'Using host key of type %s' % hostkeytype
-
-	Info = collections.namedtuple("Info", "username, hostname, password, port, hostkeytype, hostkey")
-	return Info(username, hostname, password, port, hostkeytype, hostkey)
-
-def add_workstaion():
-	"""upload command to workstation"""
-
-def download_file(filename):
-	"""download file to workstation"""
-	info = get_user_info()
-	t = paramiko.Transport((info.hostname,info.port))
-	t.connect(username=info.username, password=info.password, hostkey=info.hostkey)
-	sftp = paramiko.SFTPClient.from_transport(t)
-	sftp.get("autoEM/"+filename, filename)
-	sftp.close()
-	t.close()
-
-def upload_file(filename):
-	"""upload file to workstation"""
-	info = get_user_info()
-	t = paramiko.Transport((info.hostname,info.port))
-	t.connect(username=info.username, password=info.password, hostkey=info.hostkey)
-	sftp = paramiko.SFTPClient.from_transport(t)
-	sftp.put(filename, "autoEM/" + filename)
-	sftp.close()
-	t.close()
 
 def main():
 	#"""the main function of autoEM"""
@@ -175,36 +69,40 @@ def main():
 						option_list=option_list)
 	options, args = parser.parse_args()
 
-	if options.add:
-		add_workstaion()
-		sys.exit(0)
 	if options.version:
 		version()
 		sys.exit(0)
-	if options.upload:
-		#here we assume the upload file is placed with program file
-		program_dir = os.getcwd()
-		uploadfile = os.path.join(program_dir, options.upload)
-		if os.path.isfile(uploadfile) and not os.path.islink(uploadfile):
-			if not uploadfile.endswith(".son"):
-				print("The file you specify may not be a sonnet file, upload will continue.")
-			upload_file(options.upload)
-		else:
-			sys.stderr.write("Can't find the upload file\n")
-			sys.exit(1)
-		sys.exit(0)
-	if options.download:
-		#also, download the file to folder with program
-		download_file(options.download)
-		sys.exit(0)
-	if options.run:
-		#run the specify command, this command will delete extension automatically
-		sonfile = os.path.splitext(options.run)[0]
-		exec_command(sonfile)
-		sys.exit(0)
-	if len(args) == 0:
-		sys.stderr.write("No specify action to be done.\n")
-		sys.exit(1)
+
+	from autoEM.gui import autoEMGui
+	autoEMGui()
+	sys.exit()
+	#if options.add:
+	#	add_workstaion()
+	#	sys.exit(0)
+	#if options.upload:
+	#	#here we assume the upload file is placed with program file
+	#	program_dir = os.getcwd()
+	#	uploadfile = os.path.join(program_dir, options.upload)
+	#	if os.path.isfile(uploadfile) and not os.path.islink(uploadfile):
+	#		if not uploadfile.endswith(".son"):
+	#			print("The file you specify may not be a sonnet file, upload will continue.")
+	#		upload_file(options.upload)
+	#	else:
+	#		sys.stderr.write("Can't find the upload file\n")
+	#		sys.exit(1)
+	#	sys.exit(0)
+	#if options.download:
+	#	#also, download the file to folder with program
+	#	download_file(options.download)
+	#	sys.exit(0)
+	#if options.run:
+	#	#run the specify command, this command will delete extension automatically
+	#	sonfile = os.path.splitext(options.run)[0]
+	#	exec_command(sonfile)
+	#	sys.exit(0)
+	#if len(args) == 0:
+	#	sys.stderr.write("No specify action to be done.\n")
+	#	sys.exit(1)
 		
 	
 if __name__ == '__main__':
@@ -215,3 +113,4 @@ if __name__ == '__main__':
 			print '\nAborted.'
 		else:
 			print 'error:', e
+
